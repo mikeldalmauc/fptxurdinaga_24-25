ef3d3abc07c5b94e9124157d399ade45e2701da4
ik_lanm <ik_lanm@H015110.NET>
2024-10-16 19:32:44 +0200

    backup

---
+++ lanbide/script_backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Ruta de destino del backup
+BACKUP_DIR="/mnt/backup"
+LOG_FILE="$BACKUP_DIR/backup.log"
+
+# Comprobar si la partición de backup está montada
+if mountpoint -q "$BACKUP_DIR"; then
+    # Ejecutar rsync para hacer el backup del sistema
+    rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} / "$BACKUP_DIR" > "$LOG_FILE" 2>&1
+else
+    echo "Error: El directorio de backup no está montado." >> "$LOG_FILE"
+fi
